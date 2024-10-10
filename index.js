a6578d81511b91429244ab8d676c1a8854796a57
JessicaGrotto <jessicalgrotto@gmail.com>
2024-10-10 09:06:26 +0200

    primera parte

--- index.js
+++ index.js
@@ -1,32 +1,60 @@
 // PARA ESTE EJERCICIO SE PUEDE UTILIZAR SOLO PUBLIQUE FOR O WHILE
 
 // Crea un array vacío llamado `stack`
-
+const stack = []
 // Crea un array `numbers` con los números del 1 al 5 in orden ascendente
-
+const numbers = [1,2,3,4,5]
 // La función multiplyByTwo tiene que devolver un array con los números multiplicados por 2 de cada elemento del array `numbers`
 function multiplyByTwo(numbers) {
   // Aquí va tu código
+  let multiplicados = []
+  for(i=0; i<numbers.length; i++) {
+    multiplicados[i] = numbers[i]*2
+  }
+  return multiplicados;
 }
+
 
 // La función evenNumbers tiene que devolver los números pares del array `numbers`
 function evenNumbers(numbers) {
   // Aquí va tu código
+  let numPares = []
+  for(i=0; i<numbers.length; i++){
+    if(numbers[i]%2==0){
+      numPares[numPares.length] = numbers[i]
+    }
+  }
+  return numPares;
 }
 
 // La función numbersBackwards tiene que devolver los números del array `numbers` en orden inverso
 function numbersBackwards(numbers) {
   // Aquí va tu código
+  const inversNumbers = []
+  for(i=numbers.length; i>numbers.length; i--){
+    inversNumbers[i] = numbers[i]
+  }
+  return inversNumbers;
 }
 
 // La función `sum` tiene que devolver la suma de todos los números del array `numbers`
 function sum(numbers) {
-  // Aquí va tu código
+   //Aquí va tu código
+  let suma = 0
+  for(i=0; i<numbers.length; i++){
+  suma = suma + numbers[i]
+  }
+return suma
 }
 
 // La función `average` tiene que devolver el promedio de todos los números del array `numbers`
 function average(numbers) {
   // Aquí va tu código
+  let promedio = 0
+  for(i=0; i<numbers.length; ){
+    promedio = promedio + numbers[i]
+  }
+  return promedio / promedio.length;
 }
 
 // La función `biggestPrime` tiene que devolver el número primo más grande de los primeros 100 números
@@ -36,13 +64,14 @@ function biggestPrime(numbers) {
 }
 
 // Crea una variable person y asígnale un objeto vacío
-
+const person = {}
 // Agrega una propiedad `name` al objeto y asígnale "Jorge" como valor
-
+person.name = "Jorge"
 // Agrega una propiedad `lastName` al objeto y asígnale "Gonzalez" como valor
-
+person.lastName = "Gonzalez"
 // Agrega una propiedad `birthYear` al objeto y asígnale 1995 como valor
-
+person.birthYear = 1995
 // Agrega una función `getFullName` al objeto que devuelva el nombre completo
-
+person.getFullName = function(){return console.log(person.name + person.lastName)}
 // Agrega una función `getBirthYear` al objeto que devuelva la edad (asumiendo que es el año 2023)
+person.getBirthYear = function(){return 2023 - person.birthyear}
